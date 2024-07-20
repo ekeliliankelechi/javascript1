fb3128a28be897e6113f389b0446af46acd9e872
ekeliliankelechi <ekeliliank04@gmail.com>
2024-07-20 16:25:21 -0700

    first commit

---
+++ firstclass.js
@@ -0,0 +1,60 @@
+//console.log("Welcome");
+//console.error("Welcome");
+//console.warm("Welcome");
+
+//let emekaage = 56;
+//emekaage = 21;
+//emekaage = 36;
+//console.log(emekaage);
+
+
+ //const emekaage 67;
+ //console.log(emekaage);
+ //let score;
+ //score = "Welcome";
+ //score = "Tony"
+ //console.log(score);
+  
+ //data types
+//  const firstname ="peter";
+ 
+//  const lastname ="obi";
+//  const age = 63;
+//  const isAdmin = false;
+//  const x = null;
+//  const y = undefined;
+//  console.log(typeof y);
+ //console.log("my name is" + firstname +
+ // lastname "I am " + age + "years old ");
+
+//console.log(`my name is ${firstname} ${lastname} I am ${age} years old, I am ${isAdmin}`);
+
+//const word = "Hello world";
+//console.log(word.length);
+//const word = "Hello world";
+//console.log(word.split(""));
+//const tools = "knife, book, pencil, crayon";
+//console.log(tools.split(","));
+
+
+// const listitems = ["BEANS", "RICE", "GARRI", 40, true, null,undefined];
+// //listitems.push("Goat", 58, "computer");
+// listitems.unshift("pen", "chalk");
+// listitems .pop();
+// console.log(listitems);
+//console.log(listitems[4]);
+const studentinfo= {
+    firstname: "Favour",
+    lastname: "Amaka",
+    age: 13,
+    hobbies:["dancing", "sports", "twerking", 89],
+    haspaidSchoolFees: true,
+    x:null,
+    iswicked: undefined,
+};
+console.log(studentinfo.hobbies[3]);
+
+
+
+
+ 
